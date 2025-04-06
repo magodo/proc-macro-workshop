3c0bbb796d18ba3e8cbe0fdb21e19c7e50062dc2
magodo <wztwcy@gmail.com>
2025-04-06 10:19:45 +1000

    Builder: exercise 1 & 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,35 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = syn::parse(input).unwrap();
+    impl_builder(&ast)
+}
 
-    unimplemented!()
+fn impl_builder(ast: &DeriveInput) -> TokenStream {
+    eprintln!("{:#?}", ast);
+    let type_name = &ast.ident;
+    let builder_type_name = format_ident!("{}Builder", type_name);
+    let gen = quote! {
+        pub struct #builder_type_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #type_name {
+            pub fn builder() -> #builder_type_name {
+                #builder_type_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+    TokenStream::from(gen)
 }
--- main.rs
+++ main.rs
@@ -1,9 +1,9 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
+use derive_builder::Builder;
 
-fn main() {}
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
