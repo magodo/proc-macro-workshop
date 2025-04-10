ce63e27adf54812eb52075eafbc5d2525d5b7efe
magodo <wztwcy@gmail.com>
2025-04-10 14:27:58 +1000

    Sorted: exercise 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -4,14 +4,22 @@ use quote::quote;
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     eprintln!("{:#?}", input);
-    let _ = args;
-    let _ = input;
+    let input = syn::parse(input).unwrap();
 
-    let ast = syn::parse(input).unwrap();
-    impl_sorted(&ast)
+    match impl_sorted(&input) {
+        Ok(ts) => ts,
+        Err(err) => err.to_compile_error().into(),
+    }
 }
 
-fn impl_sorted(ast: &syn::Item) -> TokenStream {
-    _ = ast;
-    quote! {/**/}.into()
+fn impl_sorted(input: &syn::Item) -> Result<TokenStream, syn::Error> {
+    match input {
+        syn::Item::Enum(e) => {
+            Ok(quote! {/**/}.into())
+        }
+        _ => Err(syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        )),
+    }
 }
