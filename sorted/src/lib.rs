37ef865bf9023ffcb1bf082490beb000a1a69d30
magodo <wztwcy@gmail.com>
2025-04-10 15:58:24 +1000

    Sorted: exercise 3

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::spanned::Spanned;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
     eprintln!("{:#?}", input);
     let input = syn::parse(input).unwrap();
 
@@ -13,13 +14,25 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 fn impl_sorted(input: &syn::Item) -> Result<TokenStream, syn::Error> {
-    match input {
-        syn::Item::Enum(e) => {
-            Ok(quote! {/**/}.into())
+    if let syn::Item::Enum(e) = input {
+        let variants: Vec<_> = e.variants.iter().collect();
+        for i in 0..variants.len() - 1 {
+            for j in (i + 1)..variants.len() {
+                let ident_i = &variants[i].ident;
+                let ident_j = &variants[j].ident;
+                if ident_i >= ident_j {
+                    return Err(syn::Error::new(
+                        variants[j].span(),
+                        format!("{} should sort before {}", ident_j, ident_i,),
+                    ));
+                }
+            }
         }
-        _ => Err(syn::Error::new(
+        Ok(quote! {/**/}.into())
+    } else {
+        Err(syn::Error::new(
             proc_macro2::Span::call_site(),
             "expected enum or match expression",
-        )),
+        ))
     }
 }
